2c4f618ec69270ae8c1f332f2c2ab4e8bfafa3e3
Mithcodes007 <patilmithilesh488@gmail.com>
2025-08-05 21:21:32 +0530

    initial commit

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "animals": "https://www.youtube.com/watch?v=qpgTC9MDx1o&list=RDqpgTC9MDx1o&start_radio=1&pp=ygUQYW5pbWFscyBtYXJvb24gNaAHAQ%3D%3D",
+    "prarthana": "https://www.youtube.com/watch?v=OKPAPX_fGVw&list=RDOKPAPX_fGVw&start_radio=1&pp=ygUJcHJhcnRoYW5hoAcB",
+    "madira": "https://www.youtube.com/watch?v=CgK2iGxNy7M&list=RDCgK2iGxNy7M&start_radio=1&pp=ygUGbWFkaXJhoAcB",
+    "kaam pacchis": "https://www.youtube.com/watch?v=ATf05n5LBHQ&list=RDATf05n5LBHQ&start_radio=1&pp=ygUHa2FhbSAyNaAHAQ%3D%3D"
+}
+
+
